3a6646104202697e57693db4ce01c2bdf7b4f7e5
MeongGanas <farreluken@gmail.com>
2025-01-13 09:34:21 +0800

    setup

---
+++ app/layout.js
@@ -0,0 +1,30 @@
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+    variable: "--font-geist-sans",
+    subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+    variable: "--font-geist-mono",
+    subsets: ["latin"],
+});
+
+export const metadata = {
+    title: "Tanwirah",
+    description:
+        "Wujudkan ibadah suci yang khusyuk dan berkesan dengan layanan terbaik, fasilitas lengkap, serta bimbingan profesional dari keberangkatan hingga kembali ke tanah air.",
+};
+
+export default function RootLayout({ children }) {
+    return (
+        <html lang="en">
+            <body
+                className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+            >
+                {children}
+            </body>
+        </html>
+    );
+}
---
+++ app/page.js
@@ -0,0 +1,7 @@
+export default function Home() {
+    return (
+        <div>
+            <h1 className="font-bold">Halo tanwirah</h1>
+        </div>
+    );
+}
